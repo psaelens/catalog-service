47b636155906562fd48d7cf27a258be068692630
Pierre Saelens <psaelens@gmail.com>
2025-12-04 10:36:10 +0100

    chore: upgrade to Spring Boot 3.5.8

--- src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
@@ -1,33 +1,34 @@
 package com.polarbookshop.catalogservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-				.authorizeHttpRequests(authorize -> authorize
-						.mvcMatchers("/actuator/**").permitAll()
-						.mvcMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
-						.anyRequest().hasRole("employee")
-				)
-				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-				.sessionManagement(sessionManagement ->
-						sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.csrf(AbstractHttpConfigurer::disable)
-				.build();
-	}
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
+                        .anyRequest().hasRole("employee")
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
 
 	@Bean
 	public JwtAuthenticationConverter jwtAuthenticationConverter() {
--- src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -8,10 +8,10 @@ import com.polarbookshop.catalogservice.domain.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
@@ -32,7 +32,7 @@ class BookControllerMvcTests {
 	@Autowired
 	ObjectMapper objectMapper;
 
-	@MockBean
+	@MockitoBean
 	BookService bookService;
 
 	@Test
